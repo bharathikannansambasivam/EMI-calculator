fa87b942198803b2007b21dc0cd60dcd66763b77
bharathikannansambasivam <bharathikannansambasivam@gmail.com>
2023-12-27 20:56:19 +0530

    add pie chart

--- script.js
+++ script.js
@@ -7,13 +7,12 @@ function calculateEMI() {
   var denominator = Math.pow(1 + monthlyInterestRate, loanTerm) - 1;
   var powerFactor = Math.pow(1 + monthlyInterestRate, loanTerm);
   var emi = (loanAmount * monthlyInterestRate * powerFactor) / denominator;
-
   var resultContainer = document.getElementById("result");
   resultContainer.innerHTML = "EMI: INR " + emi.toFixed(2) + " per month";
 
   var tableBody = document.getElementById("table-body");
-  tableBody.innerHTML = ""; // Clear existing table rows
-
+  tableBody.innerHTML = "";
+  var tableDiv = document.getElementById("table");
   if (
     isNaN(loanAmount) ||
     isNaN(interestRate) ||
@@ -35,10 +34,11 @@ function calculateEMI() {
       var interestCell = newRow.insertCell(2);
       var principalCell = newRow.insertCell(3);
       var remainingCell = newRow.insertCell(4);
+      var EMI = newRow.insertCell(5);
 
       var interestPayment = remainingAmount * monthlyInterestRate;
       var principalPayment = emi - interestPayment;
-
+      EMI.innerHTML = emi.toFixed(2);
       monthCell.innerHTML = i;
       loanAmountCell.innerHTML = "₹ " + remainingAmount.toFixed(0);
       interestCell.innerHTML = "₹ " + interestPayment.toFixed(0);
@@ -49,4 +49,27 @@ function calculateEMI() {
       remainingAmount = remainingAmount - principalPayment;
     }
   }
+
+  if (tableDiv.style.display === "none") {
+    tableDiv.style.display = "block";
+  } else {
+    tableDiv.style.display = "none";
+  }
+
+  var xValues = ["AMOUNT", "INTEREST_RATE"];
+  var yValues = [loanAmount, interestRate];
+  var barColors = ["#b91d47", "#00aba9"];
+
+  new Chart("myChart", {
+    type: "pie",
+    data: {
+      labels: xValues,
+      datasets: [
+        {
+          backgroundColor: barColors,
+          data: yValues,
+        },
+      ],
+    },
+  });
 }
